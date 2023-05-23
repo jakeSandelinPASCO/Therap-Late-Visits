67faa7e84e5eff32be59936946af5f207b96a8fd
Jake Sandelin <jake.sandelin@pascohh.com>
2023-05-23 12:03:11 -0600

    Therap file was udpated needed to make some column exclusions

--- sendFile.py
+++ sendFile.py
@@ -4,10 +4,17 @@ import pandas as pd
 import requests
 
 
-proc_path = filedialog.askopenfilename(title = "Select the Therap Basic ISP File")
-df = pd.read_excel(r'C:\Users\JakeSa\Downloads\ISP_Data_Basic_Report (20230120134pm).xlsx')
+#proc_path = filedialog.askopenfilename(title = "Select the Therap Basic ISP File")
+df = pd.read_excel(r'C:\Users\JakeSa\Downloads\ISP_Data_Basic_Report (20230120134pm) v2.xlsx')
 
 json_data = df.to_json()
+#thePath = "C:/Users/JakeSa/Downloads/ISP_Data_Basic_Report (20230120134pm) v2.xlsx"
+thePath = "C:\\Users\\JakeSa\Downloads\\ISP_Data_Basic_Report (20230120134pm) v2.xlsx"
+#response = requests.get("http://127.0.0.1:3000/ISPUpload/" + thePath) #Use this for testing w/o Docker
+response = requests.get("http://localhost:80/ISPUpload/" + thePath) #Use this for testing w/ Docker
+#response = requests.post('http://localhost:80/ISPUpload', json=json_data)
+#response = requests.post('http://127.0.0.1:3000/ISPUpload/:' + thePath)
 
-response = requests.post('http://localhost:5000/ISPUpload', json=json_data)
+
+#http://127.0.0.1:3000/
 print(response)
